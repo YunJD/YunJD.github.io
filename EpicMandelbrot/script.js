b9228a1a68535c1cefca20669c23b2e94595f247
Yunjian Ding <yunjianding@gmail.com>
2014-12-10 23:55:58 -0500

    Controls, and canvas size does not depend on window size.

--- EpicMandelbrot/script.js
+++ EpicMandelbrot/script.js
@@ -1,6 +1,35 @@
 (function(){
 	'use strict';
 
+	$("textarea.code-area").on('keydown', function(e) {
+		if(e.which == 9) {
+			var start = this.selectionStart;
+			var end = this.selectionEnd;
+
+			this.value = this.value.substring(0, start) + '    ' + this.value.substring(end);
+			this.selectionStart = this.selectionEnd = start + 4;
+			e.preventDefault();
+
+			return false;
+		}
+	});
+
+	$("#toggle-frag").click(function() {
+		if($("#fragment-shader").css('display') == 'none') {
+			$("#toggle-frag").text("I regret looking at this code.");
+			$("#fragment-shader").show();
+			$("#recompile").parent().show();
+		} else {
+			$("#toggle-frag").text("I'll write my own shader!");
+			$("#fragment-shader").hide();
+			$("#recompile").parent().hide();
+		}
+	});
+})();
+
+(function() {
+	'use strict';
+
 	if( !Detector.webgl ){
 		Detector.addGetWebGLMessage();
 		return false;
@@ -8,26 +37,43 @@
 
 	var renderer = new THREE.WebGLRenderer({canvas: document.getElementById("view")});
 	renderer.setClearColor( 0x000000, 1 );
+	var w, h;
 	
+
 	var camera = new THREE.OrthographicCamera(
 		0,0,0,0,1,1000
 	);
 
 	var plane = new THREE.PlaneGeometry(1, 1);
-	var mesh = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({color: 'blue'}));
+	var mesh = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({color: 'orange'}));
 	mesh.scale.y = 2;
 	mesh.translateZ(-1);
 
 	var scene = new THREE.Scene();
 	scene.add(mesh);
 
-
-	$(window).on('resize', resize);
 	resize();
 
 	function resize() {
-		var w = window.innerWidth;
-		var h = window.innerHeight;
+		//Maybe I'll comment this.  Maybe.
+		w = $("#width").val();
+		h = $("#height").val();
+		if(isNaN(w)) {
+			w = 2000;
+			$("#width").val(2000);
+		} else {
+			w = parseInt(w);
+		}
+		if(isNaN(h)) {
+			h = 2000;
+			$("#height").val(2000);
+		} else {
+			h = parseInt(h);
+		}
+
+		var $canv = $("#view").css({'margin-left': -w * 0.5, 'margin-top': -h * 0.5});
+		$canv[0].width = w;
+		$canv[0].height = h;
 
 		var asp = w / h;
 
@@ -42,4 +88,10 @@
 		renderer.setSize(w, h);
 		renderer.render(scene, camera);
 	}
+
+	function recompile() {
+	}
+
+	$("#resize").click(resize);
+	$("#recompile").click(recompile);
 })();
